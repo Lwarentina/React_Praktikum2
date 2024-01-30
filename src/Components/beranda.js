3c434217af827cbc9d03973b8bdf89f6d7851616
Lwarentina <raditya.hanarta@gmail.com>
2024-01-30 14:56:06 +0700

    seg

--- src/Components/beranda.js
+++ src/Components/beranda.js
@@ -9,7 +9,7 @@ class Beranda extends React.Component {
                     <h2 class="justify">Adiwiyata</h2>
                 </div>
                 <div class="text-center">
-                    <h5 class="px-3 justify">Adiwiyata dapat diartikan sebagai tempat yang baik dan ideal dimana dapat diperoleh segala ilmu pengetahuan dan berbagai etika yang dapat menjadi dasar manusia menuju terciptanya kesejahteraan hidup kita dan menuju kepada cita-cita pembangunan berkelanjutan.</h5>
+                    <h5 class="p-3 m-3 justify">Adiwiyata dapat diartikan sebagai tempat yang baik dan ideal dimana dapat diperoleh segala ilmu pengetahuan dan berbagai etika yang dapat menjadi dasar manusia menuju terciptanya kesejahteraan hidup kita dan menuju kepada cita-cita pembangunan berkelanjutan.</h5>
                 </div>
             </div>
         )
